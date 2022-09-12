ec9e46baf0eb4b755349e8794f5f2734edd45380
vishy08 <vishyk@live.unc.edu>
2022-09-11 21:10:18 -0400

    second

--- myapp.py
+++ myapp.py
@@ -1,9 +1,17 @@
 from flask import Flask, jsonify
+import requests, json
+
 app = Flask(__name__)
-@app.route("/myjoke", methods=["GET"])
-def mymethod():
-    joke = "Why did everyone cross the road? Ha! ha, ha!"
-    ret = {'category' : 'very funny', 'value' : joke}
-    return jsonify(ret)
+@app.route("/heartrate/last", methods=["GET"])
+
+def get_heartrate():
+    myheader = {"Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhSRFQiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkzNDg4MDQxLCJpYXQiOjE2NjE5NTIwNDF9.uk4UyLwyQeLjnoE6jxKPNCxfkzs0mFTq_09cfuyV74U"}
+    myurl = "https://api.fitbit.com/1/user/-/activities/heart/date/2022-08-24/1d/1min.json"
+    resp = requests.get(myurl, headers=myheader).json()
+    time = resp["activities-heart-intraday"]["dataset"][-1]["time"]
+    value = resp["activities-heart-intraday"]["dataset"][-1]["value"]
+    print("your most recent heart rate recorded at " + str(time) + " AM is " + str(value) + " beats per minute")
+    return jsonify(value)
+
 if __name__ == '__main__':
     app.run(debug=True)
